e5ee84e90309c369b57313373684f1c700790c26
maxencetasse <maxence.tasse@gmail.com>
2024-02-02 19:55:38 +0100

    Add webpack Add stimulus bundle and configuration

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,9 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+import { startStimulusApp } from '@symfony/stimulus-bridge';
 
-const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
+export const app = startStimulusApp(require.context(
+    '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
+    true,
+    /\.[jt]sx?$/
+));
